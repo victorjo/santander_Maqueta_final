0d7990d8483997887d6018159afccc3c866ca38a
Victor Espinosa <victor.espinosa@planetmedia.com.mx>
2013-11-13 10:16:42 -0600

    utiles para supernet, overlay y modal dialog

---
+++ js/utiles.js
@@ -0,0 +1,58 @@
+var banPop = false;
+
+function creaOverlay(){
+	var divOverlay =  $("<div>");
+	var c = $(document.body);
+	divOverlay.attr({"id":"banOverlay","class":"modal"});
+	altoTotal = c.height();
+	anchoTotal = c.width();
+	divOverlay.css({"position":"fixed","position": "fixed","top":"0","left":"0","width":anchoTotal+"px","background":"white","opacity":".8","height":"100%"});
+	c.append(divOverlay);
+	divOverlay.click(destruyeOverlay);
+	banPop = true;
+	
+}
+
+function creaModal(){
+	var c = $(document.body);
+	divContent = $("<div>");
+	divContent.attr({"id":"contentOverlay","class":"modal"});
+	divContent.css({"position":"fixed","top":"0","left":"25%","width":(anchoTotal/2)+"px","height":(altoTotal/2)+"px","border":"1px solid black","display":"block","z-index":"1000","background":"red","margin":"15% auto auto"});
+	divContent.fadeIn("slow");
+	c.append(divContent);
+}
+
+function destruyeOverlay(){
+	if(banPop){
+		$(".modal").each(function(){
+			$(this).fadeOut("slow",function(){
+				$(this).remove();
+			})
+		});
+
+		return false;
+
+	}
+}
+
+
+function shortcuts(){
+	$('body').keypress(function(e){
+    switch(e.which){
+   			case 0:
+   				destruyeOverlay();
+   			break;
+
+   			default:
+   			break;
+   		}
+
+
+	});
+}
+
+$(function(){
+	shortcuts();
+})
+
+
