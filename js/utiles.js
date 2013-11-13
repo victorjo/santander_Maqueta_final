cae04fa0f14c8901ddfbae7a7c9a816f99056ccd
unknown <axnotliztac10>
2013-11-13 14:09:02 -0600

    add overlay in widgets and calculations

--- js/utiles.js
+++ js/utiles.js
@@ -1,13 +1,14 @@
 var banPop = false;
 
-function creaOverlay(){
+function creaOverlay(color){
 	var divOverlay =  $("<div>");
 	var c = $(document.body);
 	divOverlay.attr({"id":"banOverlay","class":"modal"});
 	altoTotal = c.height();
 	anchoTotal = c.width();
-	divOverlay.css({"position":"fixed","position": "fixed","top":"0","left":"0","width":anchoTotal+"px","background":"white","opacity":".8","height":"100%"});
+	divOverlay.css({"position":"fixed","top":"0","left":"0","width":anchoTotal+"px","background":color,"opacity":".8","height":"100%",'display':'none'});
 	c.append(divOverlay);
+	divOverlay.fadeIn("slow");
 	divOverlay.click(destruyeOverlay);
 	banPop = true;
 	
