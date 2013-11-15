a924c75f24c29c3c95ba894b7f6982484e221e8d
Victor Espinosa <victor.espinosa@planetmedia.com.mx>
2013-11-14 20:06:18 -0600

    Ajuste en maqueta

--- js/utiles.js
+++ js/utiles.js
@@ -20,12 +20,22 @@ function creaModal(contenedor,tipo,contenido){
 	pos = (tipo==1)?'absolute':'fixed';
 	var c = $(contenedor);
 	divContent = $("<div>");
+
+	divCerrar = $("<div>");
+	divCerrar.attr({"id":"cerrarBtn","class":"modal"});
+	divCerrar.css({"position":"absolute","top":"0","right":"0","background":"url(../img/botones/cerrar.png) center","width":"30px","height":"30px","cursor":"pointer"});
+	divCerrar.click(destruyeOverlay);
+
 	divContent.attr({"id":"contentOverlay","class":"modal"});
 	divContent.css({"position":pos,"top":"0","left":"50%","width":(anchoTotal-140)+"px","padding":"40px","height":(altoTotal/2)+"px","border":"1px solid black","display":"block","z-index":"1000","box-shadow":"0 0 5px #ccc","overflow":"auto","background":"white","margin-left":"-","margin-top":"10%"});
 	divContent.css({"margin-left":"-"+((divContent.width()/2)+40)+"px"})
 	divContent.fadeIn("slow");
+	
+
 	c.append(divContent);
-	divContent.load("contenidos/"+contenido);
+	divContent.load("contenidos/"+contenido,function(){
+		divContent.append(divCerrar);
+	});
 
 }
 
