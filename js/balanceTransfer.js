6d34a9560de6b4703291e376a886fd1614471674
Israel Salinas <israel.salinas@planetmedia.com.mx>
2013-11-20 16:51:28 -0600

    Corrección del widget reducido de Pago de Servicios

--- js/balanceTransfer.js
+++ js/balanceTransfer.js
@@ -1,8 +1,47 @@
 	$(function(){
+	
+	        // $("#rdbtn2").click(function(){
+	     
+			//	var obj = $(".activ");
+			//	console.log(obj.next());
+			//	obj.removeClass("activ");
+			//	obj.next().addClass("activ");
+			//	obj.hide();
+			//	obj.next().show();
+			//});
+			
+		//	$("#rdbtn1").click(function(){
+	     
+		//		var obj = $(".activ");
+		//		console.log(obj.next());
+		//		obj.removeClass("activ");
+		//		obj.next().addClass("activ");
+		//		obj.hide();
+		//		obj.next().show();
+		//	});
+		
+			$("#rdbtn1").click(function(){
+				$(".2").fadeOut("fast");
+				$(".1").fadeIn("fast");
+				$("#rdbtn2").removeClass("rdselect");
+				$("#rdbtn1").addClass("rdselect");
+				
+			});
+			
+			$("#rdbtn2").click(function(){
+				$(".1").fadeOut("fast");
+				$(".2").fadeIn("fast");
+				$("#rdbtn1").removeClass("rdselect");
+				$("#rdbtn2").addClass("rdselect");
+
+			});
+
+
+	
 			$("#navegador ul li").click(function(){
 				$("#navegador").find(".active").attr("class"," ");
 				$(this).addClass('active');
-			})
+			});
 
 			$(".wra_Select3 select").ddslick({
                     width:"500"
@@ -27,7 +66,15 @@
             	$("#info2").fadeOut("slow");
             	$("#info2").fadeToggle("slow");
             	 });
+           	
+           	$(".serv_in").click(function(){
+           		$("#servlist > div > .serv_in").removeClass("servslc");
+           		$(this).toggleClass("servslc");
+           		});
+           		
+
+			
 
             	   
 
-	})
+	});
