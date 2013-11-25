ee9a3aee1f985e3f06ed2d479e89708bc7375c7d
Fernando Espinosa <axnotliztac10>
2013-11-25 17:04:38 -0600

    update to switch component

--- Domiciliacion/js/componentes.js
+++ Domiciliacion/js/componentes.js
@@ -1,6 +1,6 @@
 ﻿$(function(){
 
-           	$("#on").click(function(){
+           	/*$("#on").click(function(){
            		$("#switchbox > div").animate({
            			left:"-=45"
            			},"fast");
@@ -13,8 +13,34 @@
            			},"fast");
            		$("#form").fadeIn("slow");
            		});
-           		
+           		*/
 
            		
 });
 
+
+function switchFunc(object){
+
+  var switchO = '<div style="position:relative;width:500px;">';
+         switchO += '<div class="on">Tarjeta</div>';
+         switchO += '<div class="tope"></div>';
+         switchO += '<div class="off">Servicio</div>';
+         switchO += '</div>';
+
+  var container = $("#"+object);
+
+  container.html(switchO); 
+    container.children("div:first").find(".on").click(function(){
+    container.children("div").animate({
+    left:"-=45"
+    },"fast");
+  //$("#form").fadeOut("slow");
+  });
+
+  container.children("div:first").find(".off").click(function(){
+    container.children("div").animate({
+    left:"+=45"
+    },"fast");
+  //$("#form").fadeIn("slow");
+  });
+}
