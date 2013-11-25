c4c7615a489fe704b947d9703ede0112b8eace03
unknown <abeclopez@gmail.com>
2013-11-25 16:39:37 -0600

    Domiciliacion

---
+++ Transferencias/js/componentes.js
@@ -0,0 +1,20 @@
+﻿$(function(){
+
+           	$("#on").click(function(){
+           		$("#switchbox > div").animate({
+           			left:"-=45"
+           			},"fast");
+           		$("#form").fadeOut("slow");
+           		});
+           		
+           	 $("#off").click(function(){
+           		$("#switchbox > div").animate({
+           			left:"+=45"
+           			},"fast");
+           		$("#form").fadeIn("slow");
+           		});
+           		
+
+           		
+});
+
