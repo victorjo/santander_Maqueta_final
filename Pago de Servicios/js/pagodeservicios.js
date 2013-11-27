26ec0643e0932534f21af6bfa39e5a7ee966c52e
Fernando Espinosa <axnotliztac10>
2013-11-26 18:16:41 -0600

    commit for isra js fix

--- Pago de Servicios/js/pagodeservicios.js
+++ Pago de Servicios/js/pagodeservicios.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function(){
   dropIsra();
+
+  //Transición paso 2
+  $("#siguiente").click(function(){
+    $("#step1").fadeOut("slow",function(){
+      $("#step2").fadeIn("slow");       
+      });
+  });
+      
+  $("#siguiente2").click(function(){
+    $("#step2").fadeOut("slow",function(){
+      $("#step3").fadeIn("slow");       
+      });
+  });
+  
+  $("#btnreturn").click(function(){
+    $("#step3").fadeOut("slow",function(){
+      $("#step1").fadeIn("slow");       
+      });
+  });
 });
+
+
+		
