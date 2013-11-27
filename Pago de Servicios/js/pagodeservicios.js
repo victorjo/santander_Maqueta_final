d7d033c6932555d045abac75d7f7377fecf84af9
Israel Salinas <israel.salinas@planetmedia.com.mx>
2013-11-26 18:13:11 -0600

    asdfasdfasdf

--- Pago de Servicios/js/pagodeservicios.js
+++ Pago de Servicios/js/pagodeservicios.js
@@ -1,6 +1,16 @@
-﻿$(document).ready(function(){
-  dropIsra();
+﻿/*Funhcion switch (label de mas de dos digitos)--- Flujo: Domiciliaciones Ã³ mas */
+function switchFuncDomMas(object,descripcion1,descripcion2,tipoModulo){
 
+var switchO = '<div style="position:relative;width:230px;">';
+         switchO += '<div class="on'+tipoModulo+'">'+descripcion1+'</div>';
+         switchO += '<div class="tope'+tipoModulo+'"></div>';
+         switchO += '<div class="off'+tipoModulo+'">'+descripcion2+'</div>';
+         switchO += '</div>';
+
+  var container = $("#"+object);
+
+  container.html(switchO); 
+  
   container.children("div:first").find(".on"+tipoModulo).click(function(){
     container.children("div").animate({
     left:"-=80"
@@ -71,4 +81,3 @@
        
             	 
            
-
