c750ff2fccdaf718a84ed9bdaee730d9af852f0f
Abelito <abeclopez@gmail.com>
2013-11-29 19:34:32 -0600

    union de domiciliacion

--- widgets/max/w_5/js/domiciliacion.js
+++ widgets/max/w_5/js/domiciliacion.js
@@ -32,8 +32,8 @@ $(document).ready(function(){
 			$("#info").fadeToggle("slow");
 	 });
 
-
-	/*Transiciones de flujo (siguiente): Domiciliación de servicios*/
+	/** funcionalidad de botones Continuar y atras**/
+	/*********  INICIO Transiciones de flujo: Domiciliación de servicios **********/
 	$("#siguiente").click(function(){
 		$("#step1").fadeOut("slow",function(){
 			$("#step2").fadeIn("slow");				
@@ -60,12 +60,81 @@ $(document).ready(function(){
 			});
 	});
 
-		//Transicion entre operaciones
+	/*********  FIN Transiciones de flujo(1): Domiciliación de servicios **********/
+
+
+	/*********  INICIO Transiciones de flujo(2): Domiciliación de tarjeta de credito **********/
+	$("#siguiente4").click(function(){
+		$("#step5").fadeOut("slow",function(){
+			$("#step6").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente5").click(function(){
+		$("#step6").fadeOut("slow",function(){
+			$("#step7").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente6").click(function(){
+		$("#step7").fadeOut("slow",function(){
+			$("#step8").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente7").click(function(){
+		$("#step8").fadeOut("slow",function(){
+			$("#step9").fadeIn("slow");				
+			});
+	});
+
+	/*Transiciones de flujo (atras): Domiciliación de tarjeta de credito */
+	$("#regresar2").click(function(){
+		$("#step7").fadeOut("slow",function(){
+			$("#step6").fadeIn("slow");				
+			});
+	});
+
+	/*********  FIN Transiciones de flujo(2): Domiciliación de tarjeta de credito **********/
+
+	/*********  INICIO Transiciones de flujo(3) : Consulta / Modificacion y cancelacion **********/
+	$("#siguiente8").click(function(){
+		$("#step10").fadeOut("slow",function(){
+			$("#step11").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente9").click(function(){
+		$("#step11").fadeOut("slow",function(){
+			$("#step12").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente10").click(function(){
+		$("#step12").fadeOut("slow",function(){
+			$("#step13").fadeIn("slow");				
+			});
+	});
+
+	$("#siguiente11").click(function(){
+		$("#step13").fadeOut("slow",function(){
+			$("#step14").fadeIn("slow");				
+			});
+	});
+
+	/*Transiciones de flujo (atras): Consulta / Modificacion y cancelacion */
+	$("#regresar3").click(function(){
+		$("#step12").fadeOut("slow",function(){
+			$("#step11").fadeIn("slow");				
+			});
+	});
+	/*********  FIN Transiciones de flujo(3): Consulta / Modificacion y cancelacion **********/
+
+	//Transicion entre operaciones
 	$("#navegador > ul > :nth-child(1)").click(function(){
 		cambiaFlujo("#step1");
 	});
 
-	
 	$("#navegador > ul > :nth-child(2)").click(function(){
 		cambiaFlujo("#step5");
 	});
