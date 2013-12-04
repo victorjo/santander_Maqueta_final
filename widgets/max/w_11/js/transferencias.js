e222fe117d09a6f5342d103ae5a3853b6f2007ac
Abelito <abeclopez@gmail.com>
2013-12-04 07:06:56 -0600

    maquetacion de tranferencoas 50%

--- widgets/max/w_11/js/transferencias.js
+++ widgets/max/w_11/js/transferencias.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
 	omniBox("autocompleteQ4",cards);
 	omniBox("autocompleteQ5",cards);
 	switchFuncDomMas("switchbox","Si","No","Dom");
+	switchFuncDomMas("switchbox1","Si","No","Dom");
+	switchFuncDomMas("switchbox2","Si","No","Dom");
 	ocultaSteps(15);
 });
 
@@ -32,5 +34,83 @@ $("#siguiente2").click(function(){
 			});
 	});
 
+/*Transiciones de flujo (atras): Domiciliación de servicios*/
+	$("#regresar").click(function(){
+		$("#step2").fadeOut("slow",function(){
+			$("#step1").fadeIn("slow");				
+			});
+	});
 /*********  FIN Transiciones de flujo(1): Entre Cuentas 
 Santander **********/
+
+$("#siguiente3").click(function(){
+		$("#step4").fadeOut("slow",function(){
+			$("#step5").fadeIn("slow");				
+			});
+	});
+
+$("#siguiente4").click(function(){
+		$("#step5").fadeOut("slow",function(){
+			$("#step6").fadeIn("slow");				
+			});
+	});
+
+$("#regresar2").click(function(){
+		$("#step6").fadeOut("slow",function(){
+			$("#step5").fadeIn("slow");				
+			});
+	});
+
+
+//Transicion entre operaciones
+$("#navegador > ul > :nth-child(1)").click(function(){
+	cambiaFlujo("#step1");
+});
+
+$("#navegador > ul > :nth-child(2)").click(function(){
+	backPasoImg();
+	cambiaFlujo("#step4");
+});
+
+/*($("#navegador > ul > :nth-child(3)").click(function(){
+	cambiaFlujo("#step10");
+});*/
+
+function cambiaFlujo(step){
+	
+	var npasos = $(".pasos").size();
+		$(".pasos").each(function(index){
+			if((++index) == npasos){
+				$(this).fadeOut("slow",function(){
+					$(step).delay(1000).fadeIn();
+				});
+	
+			}else{
+				$(this).fadeOut("slow");
+			}
+		})
+
+}
+
+$("#btn1").on("click", function(){
+	creaOverlay("#000","#contenido",1);
+	insertHTML($("#contenido2").html(),"modalOver");
+	
+	//$("#contenido2").fadeIn();
+
+});
+
+function insertHTML(html,contenedor){
+	$("#"+contenedor).append(html);
+}
+
+function follow(){
+	/*if ($("#token").val()==0){
+		alert("Debe ingresar su numero de TOKEN");
+		
+	}*/
+	
+	destruyeOver();
+	cambiaFlujo("#step7");
+	
+}
