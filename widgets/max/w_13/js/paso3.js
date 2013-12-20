112645afe206c0fe24ef2313edeac7acc5785bc0
Abelito <abeclopez@gmail.com>
2013-12-19 18:33:30 -0600

    Integracion : pendiente

--- widgets/max/w_13/js/paso3.js
+++ widgets/max/w_13/js/paso3.js
@@ -7,6 +7,35 @@
 		scope.removeClass("liShadow").addClass("liActive");
 		dot();
 	});
+
+  /*Funcionalidades para TABS - Mis TDC Santandern*/
+  $('#tabs li a').click(function(){
+    $('#tabs li, #content .current').removeClass('current');
+    $(this).parent().addClass('current');
+    var currentTab = $(this).attr('href');
+    $(currentTab).addClass('current');
+    if(currentTab == '#tab2'){
+      $('.calendario').hide();
+      $('.oculta').hide();
+      
+    }else{
+      $('.calendario').show();
+      $('.oculta').show();
+    }
+  });
+
+  $('.radios').click(function(){
+    $('.radios').removeClass('radioSelected');
+    $(this).addClass('radioSelected');
+  });
+
+  $('.imagen').click(function(){
+    $('.imagen').removeClass('imagenSelected');
+    $(this).addClass('imagenSelected');
+  });
+  
+  /*------ Fin funcionalidades -------*/
+
 });
 
 function dot(){
