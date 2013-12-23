d80eacb121f8cac494f5f27f68e1521e6f933400
Fernando Espinosa <axnotliztac10>
2013-12-23 12:43:33 -0600

    c

--- js/widgets.js
+++ js/widgets.js
@@ -185,13 +185,13 @@ var widgets = [
 					id:14,
 					type:2,
 					order:12,
-					contentMin:'Saldos y Movimientos',
+					contentMin:'<h1>Saldos y Movimientos</h1>',
 					contentMax:''
 				},{
 					id:15,
 					type:2,
 					order:13,
-					contentMin:'Pago de Servicios',
+					contentMin:'<h1>Pago de Servicios</h1>',
 					contentMax:''
 				}
 
@@ -230,7 +230,7 @@ function posWidgets(qwerty){
 		var newWidget = $('<div>');
 		var c = fillStyles(v.type);
 		newWidget.attr("class",c);
-		if(v.id==null || v.contentMin!="") newWidget.html(v.contentMin);
+		if(v.id==null || v.contentMin!=""){ newWidget.html(v.contentMin);/*hardcode*/ newWidget.attr("data-max","true");}
 		else{
 			$.ajax({
 				url:'widgets/min/w_'+v.id+'/index.html',
@@ -238,7 +238,7 @@ function posWidgets(qwerty){
 		        dataType:'html',
 		        success: function(res){
 		        	newWidget.html(res);
-		        	$("div > div:first,.btnaceptar_wr,img",newWidget).click(function(){
+		        	$("div > div:first,.btnaceptar_wr,img,h1",newWidget).click(function(){
 						if(typeof $(this).attr("src") != 'undefined' && $(this).attr("src").indexOf("fake")==-1)  return false;
 							getInstance(newWidget);
 					});
@@ -475,7 +475,7 @@ function newWidgetInstance(obj)
 									forA.children().fadeIn('slow',function(){
 									});
 									$("[data-id='"+id+"']").click();
-									$("div > div:first,.btnaceptar_wr,img",io).click(function(){
+									$("div > div:first,.btnaceptar_wr,img,h1",io).click(function(){
 										getInstance(io);
 									});
 								}
