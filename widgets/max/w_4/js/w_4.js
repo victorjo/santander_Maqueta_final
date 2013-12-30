2a4b95fb2bfdd3b28ee54624524c65ecb16a816f
Abelito <abeclopez@gmail.com>
2013-12-30 14:24:59 -0600

    Realizacion de funcion para switch al igual que estilos

--- widgets/max/w_4/js/w_4.js
+++ widgets/max/w_4/js/w_4.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-	remplazaTxt('Pago de servicios','Seleccione de su lista de servicios registrados el que desee pagar:');
+	//remplazaTxt('Pago de servicios','Seleccione de su lista de servicios registrados el que desee pagar:');
 
 });
 
 //funcion para cambiar texto.
-function remplazaTxt(txt1,txt2){
+/*function remplazaTxt(txt1,txt2){
 	$('.titulo').html(txt1);
 	$('.subtitulo').html(txt2);
-}
+}*/
