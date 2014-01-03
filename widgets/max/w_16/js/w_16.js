26d306213cae5a657412069ff96b372314a2063f
Israel Salinas <israel.salinas@planetmedia.com.mx>
2014-01-02 19:53:23 -0600

    administracion de tarjeta de credito

---
+++ widgets/max/w_16/js/w_16.js
@@ -0,0 +1,56 @@
+var ddData = [
+    {
+        text: "Seleccionar",
+        value: 1,
+        selected: false,
+    },
+    {
+        text: "Seleccionar",
+        value: 2,
+        selected: false,
+        
+    },
+    {
+        text: "Seleccionar",
+        value: 3,
+        selected: false,
+    },
+    {
+        text: "Seleccionar",
+        value: 4,
+        selected: false,
+    }
+];
+
+$(function(){		
+        $(".w10_HPG_wra_Select select").ddslick({
+   		 data: ddData,
+   		 width: 140,
+   		 background: "#f7f7f7",
+    	imagePosition: "left",
+    	selectText: "Seleccionar",
+   		 onSelected: function (data) {
+        console.log(data);
+    	}
+		});
+
+         $(".w10_HPG_wra_Select2 select").ddslick({
+         data: ddData,
+         width: 160,
+         background: "#f7f7f7",
+        imagePosition: "left",
+        selectText: "Seleccionar",
+         onSelected: function (data) {
+        console.log(data);
+        }
+        });
+});
+
+
+
+
+
+
+
+
+
