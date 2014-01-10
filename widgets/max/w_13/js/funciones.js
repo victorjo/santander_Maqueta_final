a141522f3a6bcdf3aeb265f714681c1417554dd2
joraz89 <jramon.roblero@gmail.com>
2014-01-09 21:35:26 -0600

    Pago de tarjeta de credito...

--- widgets/max/w_13/js/funciones.js
+++ widgets/max/w_13/js/funciones.js
@@ -14,6 +14,33 @@
 			var data = {
 				colums:['','','','Pago minimo (MXP)','Para no generar<br/>intereses(MXP)','Fecha limite de pago'],
 				rowsValues:[radios,urlimg,'Elite Rewards<br/> Platino 1234****5678','999,999,999,99','1234****1890','1234****1890'],
-				numRows:30
+				numRows:30,
+				hasStatus:true,
+				configButton:true
 			}
 			Gridius($('.tablaDinamica'),data);
+
+
+			 $('.w13_PT_contenidodatos2').click(function() {
+		        cargaCalendario('switchModal()', 'calendario');
+		    });
+
+			 /*function verificaOpcion(){
+
+			 	$(".switchLabelR").click(function(){
+			 		$(".b1").css("display","none");
+			 		$(".b2").css("display","inline");
+			 		$("#rtable2").css("display","none");
+			 		$("#rtable3").css("display","none");
+			 	});
+
+			 	$(".switchLabel").click(function(){
+			 		$(".b1").css("display","inline");
+			 		$(".b2").css("display","none");
+			 		$("#rtable2").css("display","inline");
+			 		$("#rtable3").css("display","inline");
+			 		$(".jr").css("display","none");
+			 		$(".rm").css("display","none");
+			 	});
+			 }
+			 verificaOpcion();*/
